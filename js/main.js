7e76709531f10fa69ac022bff5391c05380b4e94
ashutosh <ashutosh382singh@gmail.com>
2021-05-29 17:50:39 +0530

    starting point of item lister

---
+++ js/main.js
@@ -0,0 +1,54 @@
+var form =document.getElementById('add-form');
+var items= document.getElementById('list-group');
+
+form.addEventListener('submit',addItem);
+items.addEventListener('click',removeItem);
+items.addEventListener('click',editItem);
+
+function addItem(e){
+    e.preventDefault();
+
+    //get input value 
+    var newItem= document.getElementById('Item').value;
+
+    //new item
+    var  li= document.createElement('li');
+    li.className='list-group-item';
+    li.appendChild(document.createTextNode(newItem));
+
+    //same way delete button
+    var deleteBtn= document.createElement('button');
+    deleteBtn.className='danger-button';
+    deleteBtn.appendChild(document.createTextNode('X'));
+
+    //edit button
+    var editBtn=document.createElement('button');
+    editBtn.className='edit';
+    editBtn.appendChild(document.createTextNode('Edit'));
+
+    //append both edit and X button to our list
+    li.appendChild(editBtn);
+    li.appendChild(deleteBtn);
+    
+    items.appendChild(li);
+}
+
+//removing items 
+function removeItem(e){
+    if(e.target.classList.contains('danger-button')){
+        if(confirm('Are you sure about this?')){
+            var li=e.target.parentElement;
+            items.removeChild(li);
+        }
+    }
+}
+
+//edit items
+function editItem(e){
+    if(e.target.classList.contains('edit')){
+        var newItem= prompt("Please Enter the Item", "like Banana"); 
+        if(newItem!=null){
+            e.target.parentElement.innerHTML=newItem;
+        }   
+    }
+}
